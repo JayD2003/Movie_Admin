06442b185f553cd818c54a451c12a71266f3e28d
JayD2003 <jayasaidodla@gmail.com>
2024-08-31 00:20:25 +0530

    one more change in admin auth

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const Navbar = () => {
     const [isAdminAuthenticated, setIsAdminAuthenticated] = useState(false);
     const checkAdminAuthentication = async () => {
         try {
-            const response = await fetch(import.meta.env.VITE_BACKEND_API+'/admin/checklogin', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include'
+            // const response = await fetch(import.meta.env.VITE_BACKEND_API+'/admin/checklogin', {
+            //     method: 'GET',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     credentials: 'include'
 
+            // });
+            const response = await axios.get(import.meta.env.VITE_BACKEND_API+'/admin/checklogin', {
+                withCredentials: true // Include cookies with the request
             });
-            if (response.ok) {
+            if (response.status === 200) {
                 // Admin is authenticated
                 setIsAdminAuthenticated(true);
                 console.log("Admin Authentication Completed")
